ea55aaa761e1ac86c6fb6a9a2ffd68ec71a35a4d
Samanta Driuzzi <driuzzi.samanta.mayra@gmail.com>
2024-05-12 15:21:37 -0300

    Ajuste de tipo de componente

--- front/src/app/products/page.tsx
+++ front/src/app/products/page.tsx
@@ -1,14 +1,18 @@
+import React, { useEffect, useState } from "react";
 import ProductCard from "@/components/Card/ProductCard";
 import { getProducts } from "@/helpers/products/get/getProducts";
 import { Product } from "@/types/product/types";
-import { Tiro_Devanagari_Sanskrit } from "next/font/google";
 
-const TiroDevanagariSanskrit = Tiro_Devanagari_Sanskrit({
-  weight: "400",
-  subsets: ["latin"],
-});
-export const Products = async () => {
-  const products = await getProducts();
+const Products: React.FC = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const fetchedProducts = await getProducts();
+      setProducts(fetchedProducts);
+    };
+    fetchProducts();
+  }, []);
 
   return (
     <div className="mb-40">
@@ -18,10 +22,7 @@ export const Products = async () => {
           Tu navegador no soporta videos HTML5.
         </video>
       </div>
-      <div
-        style={{ fontFamily: TiroDevanagariSanskrit.className }}
-        className="text-6xl flex justify-center font-bold p-6 max-sm:text-2xl "
-      >
+      <div className="text-6xl flex justify-center font-bold p-6 max-sm:text-2xl ">
         Todos los productos en tu tienda
       </div>
       <div className="flex flex-wrap gap-8 justify-center mt-10 px-60">
