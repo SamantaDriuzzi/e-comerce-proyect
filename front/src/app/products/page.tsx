b7f97dafccec83f99e769e32779770db9b54311b
Samanta Driuzzi <driuzzi.samanta.mayra@gmail.com>
2024-05-12 15:36:18 -0300

    ajuste en un user-component

--- front/src/app/products/page.tsx
+++ front/src/app/products/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useEffect, useState } from "react";
 import ProductCard from "@/components/Card/ProductCard";
 import { getProducts } from "@/helpers/products/get/getProducts";
